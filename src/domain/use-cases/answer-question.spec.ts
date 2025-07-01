7918414e42a89fb8419ea2ac6dee6fd861a47583
Paulo Borges <dev.pauloborges@gmail.com>
2025-07-01 17:15:48 -0300

    add: repository for answers

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,10 +1,17 @@
 import { expect, test } from 'vitest'
 import { AnswerQuestionUseCase } from './answer-question'
+import { AnswersRepository } from '../repositories/answers-repository';
+import { Answer } from '../entities/answer';
 
-test('create an answer', () => {
-    const answerQuestion = new AnswerQuestionUseCase()
+test('create an answer', async () => {
+    const fakeAnswersRepository: AnswersRepository = {
+        create: async (answer: Answer) => {
+            return;
+        }
+    }
+    const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository)
 
-    const answer = answerQuestion.execute({
+    const answer = await answerQuestion.execute({
         questionId: '1',
         instructorId: '1',
         content: 'Nova resposta'
