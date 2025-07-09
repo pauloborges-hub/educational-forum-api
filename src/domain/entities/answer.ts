f3f43495a972101546bdd5242bf06e6e8eaeebd3
Paulo Borges <dev.pauloborges@gmail.com>
2025-07-09 10:11:38 -0300

    chore: ESLint config

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,61 +1,61 @@
-import { Entity } from "@/core/entities/entity"
-import { UniqueEntityID } from "@/core/entities/unique-entity-id"
-import { Optional } from "@/core/types/optional"
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Optional } from '@/core/types/optional'
 
 interface AnswerProps {
-    authorId: UniqueEntityID
-    questionId: UniqueEntityID
-    content: string
-    createdAt: Date
-    updatedAt?: Date
+  authorId: UniqueEntityID
+  questionId: UniqueEntityID
+  content: string
+  createdAt: Date
+  updatedAt?: Date
 }
 
 export class Answer extends Entity<AnswerProps> {
-    get authorId() {
-        return this.props.authorId
-    }
+  get authorId() {
+    return this.props.authorId
+  }
 
-    get questionId() {
-        return this.props.questionId
-    }
+  get questionId() {
+    return this.props.questionId
+  }
 
-    get content() {
-        return this.props.content
-    }
+  get content() {
+    return this.props.content
+  }
 
-    get createdAt() {
-        return this.props.createdAt
-    }
+  get createdAt() {
+    return this.props.createdAt
+  }
 
-    get updatedAt() {
-        return this.props.updatedAt
-    }
+  get updatedAt() {
+    return this.props.updatedAt
+  }
 
-    get excerpt() {
-        return this.content
-            .substring(0, 120)
-            .trimEnd()
-            .concat('...')
-    }
+  get excerpt() {
+    return this.content.substring(0, 120).trimEnd().concat('...')
+  }
 
-    private touch() {
-        this.props.updatedAt = new Date()
-    }
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
 
-    set content(content: string) {
-        this.props.content = content
-        this.touch()
-    }
+  set content(content: string) {
+    this.props.content = content
+    this.touch()
+  }
 
-    static create(
-        props: Optional<AnswerProps, 'createdAt'>,
-        id?: UniqueEntityID
-    ) {
-        const answer = new Answer({
-            ...props,
-            createdAt: new Date()
-        }, id)
+  static create(
+    props: Optional<AnswerProps, 'createdAt'>,
+    id?: UniqueEntityID,
+  ) {
+    const answer = new Answer(
+      {
+        ...props,
+        createdAt: new Date(),
+      },
+      id,
+    )
 
-        return answer
-    }
+    return answer
+  }
 }
--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,20 +1,18 @@
 import { AnswerQuestionUseCase } from './answer-question'
-import { AnswersRepository } from '../repositories/answers-repository';
-import { Answer } from '../entities/answer';
+import { AnswersRepository } from '../repositories/answers-repository'
+import { Answer } from '../entities/answer'
 
 test('create an answer', async () => {
-    const fakeAnswersRepository: AnswersRepository = {
-        create: async (answer: Answer) => {
-            return;
-        }
-    }
-    const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository)
+  const fakeAnswersRepository: AnswersRepository = {
+    create: async (answer: Answer) => {},
+  }
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository)
 
-    const answer = await answerQuestion.execute({
-        questionId: '1',
-        instructorId: '1',
-        content: 'Nova resposta'
-    })
+  const answer = await answerQuestion.execute({
+    questionId: '1',
+    instructorId: '1',
+    content: 'Nova resposta',
+  })
 
-    expect(answer.content).toEqual('Nova resposta')
+  expect(answer.content).toEqual('Nova resposta')
 })
